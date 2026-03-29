68a0461e6c138f5cb31d7995e755081606270036
Warley <warleysoaresr@gmail.com>
2026-03-28 23:27:13 -0300

    fix: peform action back when forbidden word detected and show which word caused the block

--- app/src/main/java/io/github/warleysr/dechainer/activities/BlockedWordActivity.kt
+++ app/src/main/java/io/github/warleysr/dechainer/activities/BlockedWordActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
@@ -13,15 +14,23 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.NoAdultContent
+import androidx.compose.material.icons.outlined.RemoveRedEye
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.blur
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import io.github.warleysr.dechainer.R
@@ -35,6 +44,7 @@ class BlockedWordActivity : ComponentActivity() {
 
         setContent {
             BlockedWordActivity(
+                forbiddenWord = intent.getStringExtra("word")!!,
                 onClose = { finishAfterTransition() }
             )
         }
@@ -42,7 +52,8 @@ class BlockedWordActivity : ComponentActivity() {
 }
 
 @Composable
-fun BlockedWordActivity(onClose: () -> Unit) {
+fun BlockedWordActivity(forbiddenWord: String, onClose: () -> Unit) {
+    var showWordBlurred by remember { mutableStateOf(true) }
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colorScheme.background
@@ -60,6 +71,17 @@ fun BlockedWordActivity(onClose: () -> Unit) {
                 textAlign = TextAlign.Center,
                 style = MaterialTheme.typography.bodyLarge
             )
+            Spacer(modifier = Modifier.height(8.dp))
+            Row( verticalAlignment =  Alignment.CenterVertically ) {
+                Text(forbiddenWord,
+                    Modifier.blur(if (showWordBlurred) 8.dp else 0.dp),
+                    fontWeight = FontWeight.Bold,
+                    style = MaterialTheme.typography.bodyMedium
+                )
+                IconButton(onClick = { showWordBlurred = !showWordBlurred }) {
+                    Icon(Icons.Outlined.RemoveRedEye, null)
+                }
+            }
             Spacer(modifier = Modifier.height(32.dp))
             Button(onClick = onClose) {
                 Text(stringResource(R.string.close))
